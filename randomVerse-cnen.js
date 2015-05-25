e4870cb385d34d59606fb59922ed0ca8e89ea2c6
tim-hub <tim_w@outlook.com>
2015-05-25 16:33:50 +0800

    misxture of cn en plugin

--- randomVerse-cnen.js
+++ randomVerse-cnen.js
@@ -43,9 +43,24 @@ verse[40]=["我们若与基督同死，也必与他同活；我们若能忍耐�
 verse[41]=["他虽然为儿子，还是因所受的苦难学了顺从。他既得以完全，就为凡顺从他的人成了永远得救的根源，并蒙神照着麦基洗德的等次称他为大祭司。","希伯来书6:8-10"];
 verse[42]=["主乃活石，固然是被人所弃的，却是被神所拣选、所宝贵的","彼得前书2:4"];
 verse[43]=["君主不能因兵多得救，勇士不能因力大得救;","诗篇33:16"];
+verse[44] = ["Jesus said to him, If you can beleve all things are possible to him who believes.","Mark 9:23"];
+verse[45]=["For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.","John 3:16"];
+verse[46]=["For the grave cannot praise thee, death can not celebrate thee: they that go down into the pit cannot hope for thy truth.   The living, the living, he shall praise thee, as I do this day: the father to the children shall make known thy truth.","Isaiah 38:18-19"];
+verse[47]=["In the way of righteousness is life, and in its pathway there is no death.","Proverbs 12:28"];
+verse[48]=["For thus saith the LORD, Ye have sold yourselves for nought; and ye shall be redeemed without money.","Isaiah 52:3"];
+verse[49]=["Behold, my servant shall deal prudently, he shall be exalted and extolled, and be very high.","Isaiah 52:13"];
+verse[50]=["But he was wounded for our transgressions, he was bruised for our iniquities: the chastisement of our peace was upon him; and with his stripes we are healed.","Isaiah 53:5"];
+verse[51]=["The Spirit of the Lord GOD is upon me; because the LORD hath anointed me to preach good tidings unto the meek; he hath sent me to bind up the brokenhearted, to proclaim liberty to the captives, and the opening of the prison to them that are bound;","Isaiah 61:1"];
+verse[52]=["To proclaim the acceptable year of the LORD, and the day of vengeance of our God; to comfort all that mourn;  To appoint unto them that mourn in Zion, to give unto them beauty for ashes, the oil of joy for mourning, the garment of praise for the spirit of heaviness; that they might be called trees of righteousness, the planting of the LORD, that he might be glorified.", "Isaiah 61:2-3"];
+verse[53]=["Doubtless thou art our father, though Abraham be ignorant of us, and Israel acknowledge us not: thou, O LORD, art our father, our redeemer; thy name is from everlasting.", "Isaiah 63:16"];
+verse[54]=[" I know thy works, that thou art neither cold nor hot: I would thou wert cold or hot.   [16]  So then because thou art lukewarm, and neither cold nor hot, I will spue thee out of my mouth.","Revelation of John 3:15-16"];
+verse[55]=[" As many as I love, I rebuke and chasten: be zealous therefore, and repent.","Revelation of John 3:19"];
+verse[56]=["We are troubled on every side, yet not distressed; we are perplexed, but not in despair;  [9] Persecuted, but not forsaken; cast down, but not destroyed;  [10] Always bearing about in the body the dying of the Lord Jesus, that the life also of Jesus might be made manifest in our body. ","2 Corinthians 4:8-10"];
+verse[57]=["Let every soul be subject unto the higher powers. For there is no power but of God: the powers that be are ordained of God. ","Romans 13:1"];
+verse[58]=["As for the person who hears my words but does not keep them, I do not judge him. For I did not come to judge the world, but to save it.","John 12:47"];
 var i = gerRandomNumbers(verse.length);
-document.write("<H1>" + verse[i][1] + "</H1>");
-document.write(verse[i][0]);
+
+document.write('<p align="center">'+verse[i][0]+"    "+"----"+verse[i][1]+'</p>');
 
 function gerRandomNumbers(x) {
 	return Math.floor(Math.random() * x)
